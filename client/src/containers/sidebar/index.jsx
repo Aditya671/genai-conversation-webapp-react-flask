4041e0bc34dc6f2ab7918645e1e27065267691ba
aditya.gupta671@gmail.com <aditya.gupta671@gmail.com>
2025-03-04 11:40:58 +0530

    convesation history and content display change

--- client/src/containers/sidebar/index.jsx
+++ client/src/containers/sidebar/index.jsx
@@ -1,25 +1,48 @@
 import { Content } from "antd/es/layout/layout"
 import { PageHeading } from "../../components/PageHeading"
 import { displayDateTimeMessage } from "../../utility/utility"
-import { useSelector } from "react-redux"
-import { isArray, size } from "lodash"
-
+import { useDispatch, useSelector } from "react-redux"
+import { isArray, isNull, isUndefined, size } from "lodash"
+import { Flex, Typography } from "antd"
+import { setSelectedConversation } from "../../store/conversations/slice"
+import { setSelectedMessagesList } from "../../store/messages/slice"
 
 export const SidebarComponent = ({
 
 }) => {
+    const dispatch = useDispatch();
     const conversationsList = useSelector((state) => state.conversations.conversationsList)
+    const messagesList = useSelector((state) => state.messages.messagesList)
     const selectedConversation = useSelector((state) => state.conversations.selectedConversation)
+    const handleConversationNameClick = (convObject) => {
+        dispatch(setSelectedConversation(convObject))
+        const convMessage = messagesList.find(msg => msg.conversationId === convObject.conversationId)
+        if(convObject && (size(messagesList) > 0) && (!isUndefined(convMessage) && !isNull(convMessage))){
+            dispatch(setSelectedMessagesList(convMessage['messages']))
+        }
+    }
     return (
         <>
         <Content style={{maxHeight:'100%', padding:'20px 6px'}} >
             <PageHeading headingLevel={3} headingText={displayDateTimeMessage()} />
+            <Flex
+                wrap='wrap'
+                justify={'center'}
+                align="center"
+                gap={8}
+                style={{margin:'8px 0', padding:'0 12px'}}
+            >
             {isArray(conversationsList) && size(conversationsList) > 0 &&
             conversationsList.map(conv => (
-                <div key={conv.conversationId}>
+                <Typography.Text
+                    type={selectedConversation.conversationId === conv.conversationId ? 'secondary' : '#000000e0'}
+                    onClick={() => handleConversationNameClick(conv)}
+                    key={conv.conversationId}
+                >
                     {conv.conversationTitle}
-                </div>
+                </Typography.Text>
             ))}
+            </Flex>
         </Content>
         </>
     )
--- client/src/store/messages/slice.js
+++ client/src/store/messages/slice.js
@@ -3,7 +3,6 @@ import { messageAvatarSrcDefault, messageTypes } from "../../helper/constants";
 import logo from '../../chatbot-preview.png';
 
 const initialState = {
-    selectedMessage: null,
     introMessage:{
         messageId: 0,
         messageType:messageTypes['bot'],
@@ -24,14 +23,12 @@ export const messageSlicer = createSlice({
     name: "messages",
     initialState,
     reducers: {
-        setSelectedMessage: (state, action) => { state.selectedMessage = action.payload },
         setMessagesList: (state, action) => { state.messagesList = action.payload },
         setSelectedMessagesList: (state, action) => { state.selectedConversationMessages = action.payload },
     },
 });
 
 export const {
-    setSelectedMessage,
     setMessagesList,
     setSelectedMessagesList
 } = messageSlicer.actions;
