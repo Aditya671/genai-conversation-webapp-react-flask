e14ebc8f126d485b09847a3f50272958e731f86e
aditya.gupta671@gmail.com <aditya.gupta671@gmail.com>
2025-06-21 15:05:10 +0530

    icons added with animation

---
+++ client/src/assets/svg/VoiceTypingFilledSVG.jsx
@@ -0,0 +1,32 @@
+export const VoiceTypingFilledSVG = () => (
+    <svg width="50" height="50" viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg">
+        {/* <circle cx="60" cy="60" r="55" stroke="black" stroke-width="3" fill="none"/> */}
+        <style>
+            {`
+            .bar {
+            transform-origin: bottom;
+            animation: bounce 1.2s infinite ease-in-out;
+            }
+            .bar2 { animation-delay: 0.2s; }
+            .bar3 { animation-delay: 0.4s; }
+            .bar4 { animation-delay: 0.6s; }
+            .bar5 { animation-delay: 0.8s; }
+
+            @keyframes bounce {
+            0%, 100% {
+                transform: scaleY(1);
+            }
+            50% {
+                transform: scaleY(1.3);
+            }
+            }
+        `}
+        </style>
+        <rect class="bar bar1" x="20" y="35" rx="8" ry="8" width="10" height="20" fill="#cacaca" />
+        <rect class="bar bar2" x="35" y="25" rx="8" ry="8" width="10" height="40" fill="#cacaca" />
+        <rect class="bar bar3" x="50" y="15" rx="8" ry="8" width="10" height="60" fill="#cacaca" />
+        <rect class="bar bar4" x="65" y="25" rx="8" ry="8" width="10" height="40" fill="#cacaca" />
+        <rect class="bar bar5" x="80" y="35" rx="8" ry="8" width="10" height="20" fill="#cacaca" />
+    </svg>
+
+)
